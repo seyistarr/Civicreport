53ab58b529fcb67d4f2728ee919c1254e5f8a04f
seyistarr <oluwaseyistarr@gmail.com>
2026-06-15 20:43:53 -0700

    Updated project

---
+++ .env.js
@@ -0,0 +1,8 @@
+const supabaseClient = supabase.createClient(
+  'https://dkpivxujzpkrgijnztqj.supabase.co',
+  'sb_publishable_ZFxG1w4BNrc6YDfGip9bxw_x9MbvHm8'
+);
+const CLOUDINARY_CLOUD_NAME = 'duzyf1kda';
+const CLOUDINARY_IMAGE_PRESET = 'image_upload';
+const CLOUDINARY_VIDEO_PRESET = 'video_upload';
+const PROTOMAPS_API_KEY = 'cf68e0871478e67f';
